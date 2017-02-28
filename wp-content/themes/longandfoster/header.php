4eb88bbccf0bec4cec71b38690074c724c06da9f
Christian Bryant <christian.bryant@mobelux.com>
2017-02-27 22:43:51 -0500

    starts work on navbar

--- wp-content/themes/longandfoster/header.php
+++ wp-content/themes/longandfoster/header.php
@@ -23,27 +23,29 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'longandfoster' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'longandfoster' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	<nav class="navbar">
+		<div class="navbar__container">
+
+			<div class="logo__container">
+				<a class="logo"><img src="<?php echo get_template_directory_uri() ?>/images/Logo@2x.png"></a>
+			</div>
+
+			<div class="navbar__page-links">
+				<a href="#">About</a>
+				<a href="#">Agents</a>
+				<a href="#">Listing</a>
+				<a href="#">Community</a>
+			</div>
+
+			<div class="navbar__contact">
+				<a href="#">Contact</a>
+			</div>
+
+		</div>
+
+	</nav>
+
 
 	<div id="content" class="site-content">
